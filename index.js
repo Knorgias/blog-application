6f0131308ae873f8671aed0753d6a6f6008e91f1
Kostas Norgias <kostasnorgias@gmail.com>
2017-03-14 15:58:38 +0100

    fixed bodyparser issue

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const session = require('express-session')
+const bodyparser = require( 'body-parser' )
+
+
 
 //My Session
 app.use( session({
@@ -15,14 +18,12 @@ app.use( session({
 const users = require( __dirname + '/routes/users')
 const posts = require(__dirname + '/routes/posts')
 
-//Using bodyparser for req,res easy access
-const bodyparser = require( 'body-parser' )
-
 //My custom modules inclusions
 const db = require( __dirname + '/modules/database' )
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'))
+app.use(bodyparser.urlencoded({ extended: false }))
 
 app.use('/', users);
 app.use('/posts', posts)
